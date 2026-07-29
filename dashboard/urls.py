f38f9b329212df8534812de11b8aad18a0f5fda1
mohamed-sinani <kapancha.tz@gmail.com>
2026-07-29 11:03:51 +0300

    Update dashboard URLs

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,6 @@ app_name="dashboard"
 urlpatterns = [
     path("",views.dashboard_home,name="home"),
     path("marketing/",views.marketing_home,name="marketing"),
+    path("marketing/<int:pk>/edit/",views.campaign_edit,name="campaign_edit"),
+    path("marketing/<int:pk>/delete/",views.campaign_delete,name="campaign_delete"),
 ]
