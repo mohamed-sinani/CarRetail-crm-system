a1b347720e19f273e3ad97d27fb4390134418919
mohamed-sinani <kapancha.tz@gmail.com>
2026-07-29 11:03:20 +0300

    Add deal tests

--- deals/tests.py
+++ deals/tests.py
@@ -1,1 +1,19 @@
 from django.test import TestCase
+from .models import Deal
+from accounts.models import User
+from customers.models import Customer
+from vehicles.models import Vehicle
+
+class DealModelTest(TestCase):
+    def setUp(self):
+        self.sales=User.objects.create_user(username="sales2",password="pass",role=User.Role.SALES)
+        self.customer=Customer.objects.create(full_name="Test Client",phone="255700000001")
+        self.vehicle=Vehicle.objects.create(brand="Kawasaki",model="Ninja 400",year=2025,price=8000,mileage=0)
+
+    def test_create_deal(self):
+        deal=Deal.objects.create(customer=self.customer,vehicle=self.vehicle,salesperson=self.sales,stage=Deal.Stage.NEW_LEAD)
+        self.assertEqual(deal.stage,Deal.Stage.NEW_LEAD)
+
+    def test_deal_str(self):
+        deal=Deal.objects.create(customer=self.customer,vehicle=self.vehicle,salesperson=self.sales)
+        self.assertIn("Test Client",str(deal))
