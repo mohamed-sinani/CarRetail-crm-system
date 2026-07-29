294cefbf5acf59760f6a536ce6d558d5c7def317
mohamed-sinani <kapancha.tz@gmail.com>
2026-07-29 11:06:08 +0300

    Add report tests

--- reports/tests.py
+++ reports/tests.py
@@ -1,1 +1,7 @@
 from django.test import TestCase
+from .models import ReportSnapshot
+
+class ReportSnapshotModelTest(TestCase):
+    def test_create_snapshot(self):
+        r=ReportSnapshot.objects.create(report_type=ReportSnapshot.ReportType.SALES,title="Monthly Sales",payload={"count":5})
+        self.assertEqual(str(r),"Monthly Sales")
