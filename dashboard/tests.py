9a82091fd9e0ab6ff9ad19ad4f9c5f022e971e13
mohamed-sinani <kapancha.tz@gmail.com>
2026-07-29 11:04:02 +0300

    Add marketing campaign tests

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,1 +1,9 @@
 from django.test import TestCase
+from .models import MarketingCampaign
+
+class MarketingCampaignModelTest(TestCase):
+    def test_create_campaign(self):
+        c=MarketingCampaign.objects.create(title="Summer Sale",channel=MarketingCampaign.Channel.FACEBOOK,message="Big discounts!")
+        self.assertEqual(str(c),"Summer Sale")
+        self.assertEqual(c.channel,"FACEBOOK")
+        self.assertEqual(c.views,0)
