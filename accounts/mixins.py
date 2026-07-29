84f7c34ca8ecc0b757d89e5072f754f5fceab5be
mohamed-sinani <kapancha.tz@gmail.com>
2026-07-29 11:00:41 +0300

    Update RoleRequiredMixin for new role

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -4,9 +4,9 @@ from django.shortcuts import redirect
 class RoleRequiredMixin(LoginRequiredMixin):
     allowed_roles=()
     def dispatch(self,request,*args,**kwargs):
-        if notrequest.user.is_authenticated:
-            returnsuper().dispatch(request,*args,**kwargs)
-        if request.user.is_superuserorrequest.user.roleinself.allowed_roles:
-            returnsuper().dispatch(request,*args,**kwargs)
+        if not request.user.is_authenticated:
+            return super().dispatch(request,*args,**kwargs)
+        if request.user.is_superuser or request.user.role in self.allowed_roles:
+            return super().dispatch(request,*args,**kwargs)
         messages.error(request,"You do not have permission to access that section.")
-        returnredirect("dashboard:home")
+        return redirect("dashboard:home")
