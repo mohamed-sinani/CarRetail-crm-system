0652399b67856349364948156f62062d36fc512b
mohamed-sinani <kapancha.tz@gmail.com>
2026-07-29 11:06:34 +0300

    Update public URLs with chat routes

--- public/urls.py
+++ public/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('',views.home,name='public_home'),
-    path('motorcycle/<int:pk>/',views.vehicle_detail,name='public_vehicle_detail'),
+    path('car/<int:pk>/',views.vehicle_detail,name='public_vehicle_detail'),
+    path('car/<int:pk>/chat/',views.chat_room,name='public_chat'),
+    path('register/',views.register,name='public_register'),
+    path('login/',views.customer_login,name='public_login'),
+    path('logout/',views.customer_logout,name='public_logout'),
 ]
