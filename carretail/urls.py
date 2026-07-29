429f8b6b9ca7eefd1e16db445c19e475249f9a85
mohamed-sinani <kapancha.tz@gmail.com>
2026-07-29 11:01:38 +0300

    Update root URL config

--- carretail/urls.py
+++ carretail/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('sales/',include('sales.urls')),
     path('announcements/',include('announcements.urls')),
     path('reports/',include('reports.urls')),
+    path('messages/',include('inbox.urls')),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
