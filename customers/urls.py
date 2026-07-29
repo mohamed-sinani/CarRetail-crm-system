7191c9c77b8e41e936cade00fff2a5801b03605a
mohamed-sinani <kapancha.tz@gmail.com>
2026-07-29 15:35:20 +0300

    Add customer detail route

--- customers/urls.py
+++ customers/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CustomerCreateView,CustomerDeleteView,CustomerListView,CustomerUpdateView
+from .views import CustomerCreateView,CustomerDeleteView,CustomerDetailView,CustomerListView,CustomerUpdateView
 app_name="customers"
 urlpatterns = [
     path("",CustomerListView.as_view(),name="list"),
     path("add/",CustomerCreateView.as_view(),name="add"),
+    path("<int:pk>/",CustomerDetailView.as_view(),name="detail"),
     path("<int:pk>/edit/",CustomerUpdateView.as_view(),name="edit"),
     path("<int:pk>/delete/",CustomerDeleteView.as_view(),name="delete"),
 ]
