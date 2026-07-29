2ef00814ae85d5b3a5c241e4aef6c817335eb9e3
mohamed-sinani <kapancha.tz@gmail.com>
2026-07-29 11:01:30 +0300

    Fix settings context processor name

--- carretail/settings.py
+++ carretail/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'sales',
     'reports',
     'announcements',
+    'inbox',
     'public',
 ]
 MIDDLEWARE = [
@@ -41,6 +42,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'inbox.context_processors.inquiry_count',
             ],
         },
     },
@@ -83,3 +85,4 @@ STATIC_URL='static/'
 STATICFILES_DIRS=[BASE_DIR/'static']
 MEDIA_URL='media/'
 MEDIA_ROOT=BASE_DIR/'media'
+DEALER_PHONE='+254700000000'
