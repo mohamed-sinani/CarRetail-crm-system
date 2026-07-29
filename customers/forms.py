1a5112a4cc48528b6e456460efc46cbe85e1f8a6
mohamed-sinani <kapancha.tz@gmail.com>
2026-07-29 11:02:21 +0300

    Update customer forms

--- customers/forms.py
+++ customers/forms.py
@@ -36,7 +36,7 @@ class CustomerForm(forms.ModelForm):
         if commit:
             customer.save()
             self.save_m2m()
-        returncustomer
+        return customer
     def _get_or_create_salesperson(self,name):
         User=get_user_model()
         parts=name.split()
@@ -44,7 +44,7 @@ class CustomerForm(forms.ModelForm):
         last_name=" ".join(parts[1:])
         base_username=slugify(name).replace("-",".")or"salesperson"
         existing_user=User.objects.filter(role=User.Role.SALES,username__iexact=base_username).first()
-        if not existing_userandlast_name:
+        if not existing_user and last_name:
             existing_user = User.objects.filter(
                 role=User.Role.SALES,
                 first_name__iexact=first_name,
@@ -53,7 +53,7 @@ class CustomerForm(forms.ModelForm):
         if not existing_user:
             existing_user=User.objects.filter(role=User.Role.SALES,first_name__iexact=name).first()
         if existing_user:
-            returnexisting_user
+            return existing_user
         username=base_username
         counter=1
         while User.objects.filter(username=username).exists():
@@ -62,4 +62,4 @@ class CustomerForm(forms.ModelForm):
         user=User(username=username,first_name=first_name,last_name=last_name,role=User.Role.SALES)
         user.set_unusable_password()
         user.save()
-        returnuser
+        return user
