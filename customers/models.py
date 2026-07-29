64c3a460a421e4bb4adf455abde3759525193085
mohamed-sinani <kapancha.tz@gmail.com>
2026-07-29 15:33:45 +0300

    Add customer value properties (total_spent, total_deals_value, total_value) to Customer model

--- customers/models.py
+++ customers/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.db.models import Sum
 class Customer(models.Model):
     full_name=models.CharField(max_length=140)
     phone=models.CharField(max_length=30)
@@ -18,3 +19,12 @@ class Customer(models.Model):
         ordering=["full_name"]
     def __str__(self):
         return self.full_name
+    @property
+    def total_spent(self):
+        return self.sales.aggregate(total=Sum("amount"))["total"] or 0
+    @property
+    def total_deals_value(self):
+        return self.deals.aggregate(total=Sum("expected_value"))["total"] or 0
+    @property
+    def total_value(self):
+        return self.total_spent + self.total_deals_value
